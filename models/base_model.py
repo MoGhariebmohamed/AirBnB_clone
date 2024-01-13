564e48f4cb2da03c3dd8f778f3630c079ee8d467
mohamed gharieb <m.gharieb89@gmail.com>
2024-01-13 04:16:00 -0800

    t3 v1

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import json
+import uuid
+from datetime import datetime
+""" class base model for the project """
+""" for base class"""
+
+
+class BaseModel:
+    """defines all common attributes/methods for other classes"""
+
+    def __init__(self):
+        """
+        string - assign with an uuid when an instance is created
+            args:
+                generate unique
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+    def save(self):
+        """updates the public instance attribute"""
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values"""
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
+
+    def __str__(self):
+        """to print the instance as a string representation"""
+        return "[{}] ({}) ({})".format(self.__class__.__name__,
+                                       self.id, self.__dict__)
