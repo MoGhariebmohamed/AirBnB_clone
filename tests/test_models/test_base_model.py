9d4b751680d18b069438749d3f0df38256f7e7e4
mohamed gharieb <m.gharieb89@gmail.com>
2024-01-14 05:59:36 -0800

    t3 v8

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -24,11 +24,27 @@ class Test_Base_Model(unittest.TestCase):
         self.assertIsNotNone(self.testModel.id)
         self.assertIsNotNone(self.testModel.created_at)
         self.assertIsNotNone(self.testModel.updated_at)
+
     def test_save(self):
         """to test save method output."""
         vlaue_updated_at = self.testModel.updated_at
         value_now_updated_at = self.testModel.save()
         self.assertNotEqual(vlaue_updated_at, value_now_updated_at)
+
+    def test_to_dict(self):
+        """to test dictionary method output."""
+        Test_My_Model = self.testModel.to_dict()
+        self.assertIsInstance(Test_My_Model, dict)
+        self.assertEqual(Test_My_Model["__class__"], 'BaseModel')
+        self.assertEqual(Test_My_Model['id'], self.testModel.id)
+        self.assertEqual(Test_My_Model['created_at'], self.testModel.created_at.isoformat())
+        self.assertEqual(Test_My_Model["updated_at"], self.testModel.updated_at.isoformat())
+
+    def test_str(self):
+        """to test print format method representation."""
+        self.assertTrue(str(self.testModel).startswith('[BaseModel]'))
+        self.assertIn(self.testModel.id, str(self.testModel))
+        self.assertIn(str(self.testModel.__dict__), str(self.testModel))
 
 
 if __name__ == "__main__":
