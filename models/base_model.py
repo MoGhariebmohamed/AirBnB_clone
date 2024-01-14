a1a5abfdc033fbf361b135e65634d47e9cfb488d
mohamed gharieb <m.gharieb89@gmail.com>
2024-01-14 10:30:55 -0800

    t4 v00

--- models/base_model.py
+++ models/base_model.py
@@ -8,15 +8,25 @@ from datetime import datetime
 class BaseModel:
     """defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         string - assign with an uuid when an instance is created
-            args:
-                generate unique
+        args:
+            args: not be used
+            kwargs: each key of this dictionary is an attribute name
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """updates the public instance attribute"""
