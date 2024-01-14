4f32c7ea9a171cc362b1eb3be41c54db2c146f84
mohamed gharieb <m.gharieb89@gmail.com>
2024-01-14 05:36:15 -0800

    t3 v6

--- models/base_model.py
+++ models/base_model.py
@@ -26,6 +26,7 @@ class BaseModel:
         """returns a dictionary containing all keys/values"""
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
 
