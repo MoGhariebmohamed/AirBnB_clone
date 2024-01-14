047632112ad2f2c86c3f6149cf27345626bb04c9
rania <rania14fathallah@gmail.com>
2024-01-14 17:53:35 +0100

    1

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,20 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
+        
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+        
 
     def save(self):
         """updates the public instance attribute"""
