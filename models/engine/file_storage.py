13faeebab40f928a2095d2a3bb493f54ba108655
mohamed gharieb <m.gharieb89@gmail.com>
2024-01-14 13:32:33 -0800

    t5 v4

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,12 @@
 import json
 from os.path import exists
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
 
 
 class FileStorage:
@@ -11,33 +18,33 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-
     def all(self):
         """Returns the dictionary __objects."""
-        return self.__objects
-
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
-
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)."""
         serialized_objects = {key: obj.to_dict() for key,
                               obj in self.__objects.items()}
-        with open(self.__file_path, 'w') as file:
-                  json.dump(serialized_objects, file)
-
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects(only if the file exists)."""
-        if exists(self.__file_path):
-            with open(self.__file_path, 'r') as file:
-                data = json.load(file)
-                for key, obj_data in data.items():
-                    class_name, obj_id = key.split('.')
-                    class_ = globals()[class_name]
-                    obj = class_(**obj_data)
-                    self.__objects[key] = obj
+        if exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as file:
+                try:
+                    data = json.load(file)
+
+                    for key, obj_data in data.items():
+                        class_name, obj_id = key.split('.')
+                        class_ = eval(class_name)
+                        obj = class_(**obj_data)
+                        FileStorage.__objects[key] = obj
+                except Exception:
+                    pass
