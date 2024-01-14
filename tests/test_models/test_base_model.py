fb97bb8dea3ea934e7abb801e914de4fe380b108
mohamed gharieb <m.gharieb89@gmail.com>
2024-01-14 05:02:57 -0800

    t3 v4

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,22 +5,26 @@ from models.base_model import BaseModel
 from datetime import datetime
 import sys
 
+
 class Test_Base_Model(unittest.TestCase):
-    """This class for unittest.""" 
+    """This class for unittest."""
+
     @classmethod
     def setUpClass(cls):
         """Runs this setup before each test."""
         cls.testModel = BaseModel()
 
     @classmethod
-    def  tearDownClass(cls):
+    def tearDownClass(cls):
         """Teardown the basemodel."""
         del cls.testModel
-    
+
     def test_init(self):
         """Test the init function"""
         self.assertIsNotNone(self.testModel.id)
         self.assertIsNotNone(self.testModel.created_at)
         self.assertIsNotNone(self.testModel.updated_at)
+
+
 if __name__ == "__main__":
     unittest.main()
