701b5a3c973201a39c98981f18e0c53ca4037c21
mohamed gharieb <m.gharieb89@gmail.com>
2024-01-14 17:42:11 -0800

    t6 v1

--- console.py
+++ console.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+""" contains the entry point of the command interpreter."""
 import cmd
 
 
@@ -7,6 +9,10 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Exit the command interpreter."""
         return True
+
+    def do_help(self, arg):
+        """help for the command interpreter."""
+        print("Quit command to exit the program")
 
     def do_EOF(self, arg):
         """Exit the command interpreter."""
